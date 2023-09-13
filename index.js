806e95213b31e2024645788b24015bfebb767847
Sanketh-Ganiga <sankethganiga888@gmail.com>
2023-09-13 21:05:03 +0530

    code debugger

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post("/convert",async(req,res)=>{
         },
         data:{
             model:"gpt-3.5-turbo-0613",
-            messages:[{role:"user",content:`Act as a Code Convertor,Just Convert the ${code} into ${language}`}],
+            messages:[{role:"user",content:`Act as a Code Convertor,Just Convert the ${code} into ${language}.Don't give any explanations`}],
             max_tokens:1000,
         }
     }
@@ -40,6 +40,37 @@ app.post("/convert",async(req,res)=>{
     }
 })
 
+
+
+app.post("/debug",async(req,res)=>{
+    const {code}=req.body
+    const options={
+        method:"POST",
+        url:"https://api.openai.com/v1/chat/completions",
+        headers:{
+          "Content-Type":"application/json",
+          "Authorization":`Bearer ${API_KEY}`
+        },
+        data:{
+            model:"gpt-3.5-turbo-0613",
+            messages:[{role:"user",content:`Act as a Code Debugger,Just Debug the ${code} and explain if any error or problems exist and provide the solution as well`}],
+            max_tokens:1000,
+        }
+    }
+    try{
+        const response = await axios(options)
+        const data = response.data
+        console.log(data)
+        console.log(data.choices[0].message.content)
+        res.send(data.choices[0].message.content)
+        
+       
+    
+
+    }catch(err){
+        console.log(err)
+    }
+})
 app.listen(8080,()=>{
     console.log("listening to port 8080")
 })
