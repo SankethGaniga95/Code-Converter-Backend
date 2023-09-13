11d94032b8476a87967a4611eb89580c54a4b342
Sanketh-Ganiga <sankethganiga888@gmail.com>
2023-09-13 21:19:40 +0530

    quality assess

--- index.js
+++ index.js
@@ -71,6 +71,37 @@ app.post("/debug",async(req,res)=>{
         console.log(err)
     }
 })
+
+
+app.post("/quality",async(req,res)=>{
+    const {code}=req.body
+    const options={
+        method:"POST",
+        url:"https://api.openai.com/v1/chat/completions",
+        headers:{
+          "Content-Type":"application/json",
+          "Authorization":`Bearer ${API_KEY}`
+        },
+        data:{
+            model:"gpt-3.5-turbo-0613",
+            messages:[{role:"user",content:`Act as a Code Assessment Expert,Just Evaluate the ${code} and give a detailed report. Evaluation must be based on the following parameter,Code Consistency,Code Performance,Code Documentation,Error Handling,Code Testability,Code Modularity,Code Complexity,Code Duplication,Code Readability. At the end, give percentage wise evaluation of the parameter ranging from 0% to 100%`}],
+            max_tokens:1000,
+        }
+    }
+    try{
+        const response = await axios(options)
+        const data = response.data
+        console.log(data)
+        console.log(data.choices[0].message.content)
+        res.send(data.choices[0].message.content)
+        
+       
+    
+
+    }catch(err){
+        console.log(err)
+    }
+})
 app.listen(8080,()=>{
     console.log("listening to port 8080")
 })
